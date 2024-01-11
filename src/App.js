e7e2c2cf95f9163c58618327fae628e062697073
Dipendra singh <dipendravishnu@gmail.com>
2024-01-11 11:58:44 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./index.css"
+import { Routes,Route } from "react-router-dom"
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='bg-[#711DB0]'>
+        <Navbar/>
+      </div>
+      
+      <Routes>
+        <Route path="/" element ={<Home/>} />
+        <Route path="/Cart" element ={<Cart/>} />
+      </Routes>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Spinner.css"
+const Spinner = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+        <div className='spinner '>
+
+            </div>
+    </div>
+  )
+}
+
+export default Spinner
